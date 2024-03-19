03ba776d9859c27f84c0dd1720354f1f3bc6ab8b
BayBreezy <behon.baker@yahoo.com>
2024-03-19 06:45:33 -0500

    chore: update composable

--- src/runtime/useRellax.ts
+++ src/runtime/useRellax.ts
@@ -1,15 +1,38 @@
 import Rellax, { type RellaxOptions } from "rellax";
-import { onUpdated, onUnmounted, ref } from "vue";
+import { onUpdated, onUnmounted, ref, onMounted } from "vue";
 
 export const useRellax = (el?: string | Element | undefined, options?: RellaxOptions) => {
   if (typeof window === "undefined") return;
 
-  const instance = ref(new Rellax(el, options));
-  onUpdated(() => {
-    instance?.value?.refresh();
+  const instance = ref<InstanceType<typeof Rellax> | null>(null);
+
+  const initializeRellax = () => {
+    instance.value = new Rellax(el, options);
+  };
+
+  const destroyRellax = () => {
+    if (instance.value) {
+      instance.value.destroy();
+      instance.value = null;
+    }
+  };
+
+  // Initialize Rellax when component is mounted
+  onMounted(() => {
+    initializeRellax();
   });
+
+  // Destroy the instance if the element is removed from the DOM
   onUnmounted(() => {
-    instance?.value?.destroy();
+    destroyRellax();
   });
+
+  // Reinitialize Rellax when component is updated
+  onUpdated(() => {
+    if (!instance.value) {
+      initializeRellax();
+    }
+  });
+
   return instance;
 };
