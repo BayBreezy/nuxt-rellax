b75f13411fd27ee68c31e0d78dbd458001e050c5
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-05-01 11:38:05 -0500

    style: reorganize logo configuration in app.config.ts for clarity

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,8 +1,15 @@
 export default defineAppConfig({
   docd: {
     ui: {
+      expandNav: true,
       borderType: "dashed",
       header: {
+        logo: {
+          alt: "Nuxt Rellax Logo",
+          dark: "/logo-white.png",
+          light: "/logo.png",
+          favicon: "/favicon.ico",
+        },
         title: "Nuxt Rellax",
       },
       transition: {
